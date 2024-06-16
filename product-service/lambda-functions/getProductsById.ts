40a2b67740d1af5c83b4d1d6ec7691466d1e89ad
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-16 21:14:48 +0500

    fix: fix CORS error

--- product-service/lambda-functions/getProductsById.ts
+++ product-service/lambda-functions/getProductsById.ts
@@ -1,26 +1,40 @@
 import { products } from "./products";
 
-exports.handler = async (event: { pathParameters: { id: string; }; }) => {
+exports.handler = async (event: { pathParameters: { id: string } }) => {
   const id = event.pathParameters?.id;
 
   if (!id) {
     return {
       statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
+      },
       body: JSON.stringify({ message: "Missing path parameter: id" }),
     };
   }
 
-  const product = products.find(item => item.id === id);
+  const product = products.find((item) => item.id === id);
   if (!product) {
     return {
       statusCode: 404,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
+      },
       body: JSON.stringify({ message: "Product not found" }),
     };
   }
 
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
+    },
     body: JSON.stringify(product),
   };
 };
--- product-service/lambda-functions/getProductsList.ts
+++ product-service/lambda-functions/getProductsList.ts
@@ -3,7 +3,11 @@ import { products } from "./products";
 exports.handler = async () => {
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
+    },
     body: JSON.stringify(products),
   };
 };
