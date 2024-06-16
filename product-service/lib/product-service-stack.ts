2ab322f143e6a4927a4b7a3dfb506398b7049296
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-16 16:42:16 +0500

    feat: config lambda with api gateway

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -2,30 +2,43 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { Cors, LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Define the Lambda function resource
-    const productsListFunction = new lambda.Function(
-      this,
-      "ProductsListFunction",
-      {
-        runtime: lambda.Runtime.NODEJS_20_X,
-        code: lambda.Code.fromAsset("lambda-functions"),
-        handler: "getProductsList.handler",
-      }
-    );
-
-    // Define the API Gateway resource
-    const api = new apigateway.LambdaRestApi(this, "ProductsListApi", {
-      handler: productsListFunction,
-      proxy: false,
+    // Create our API Gateway
+    const api = new RestApi(this, "ProductsRestAPI", {
+      restApiName: "ProductsRestAPI",
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
     });
 
-    // Define the '/hello' resource with a GET method
-    const productsResource = api.root.addResource("products");
-    productsResource.addMethod("GET");
+    // Create our Lambda functions to handle requests
+    const productsLambda = new NodejsFunction(this, "ProductsLambda", {
+      entry: "lambda-functions/getProductsList.ts",
+      handler: "handler",
+    });
+
+    const productLambda = new NodejsFunction(this, "ProductLambda", {
+      entry: "resources/endpoints/getProductsById.ts",
+      handler: "handler",
+    });
+
+    // Define our API Gateway endpoints
+    const products = api.root.addResource("posts");
+    const product = products.addResource("{id}");
+
+    // Connect our Lambda functions to our API Gateway endpoints
+    const productsIntegration = new LambdaIntegration(productsLambda);
+    const productIntegration = new LambdaIntegration(productLambda);
+
+    // Define our API Gateway methods
+    products.addMethod("GET", productsIntegration);
+    product.addMethod("GET", productIntegration);
   }
 }
