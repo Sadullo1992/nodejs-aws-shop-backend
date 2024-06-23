e26ab6b56cf3b904547e1316233e7652de8562a3
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-23 10:08:12 +0500

    feat: init dynamodb tables

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -2,12 +2,32 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Cors, LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create our DynamoDB tables
+    const productsTable = new Table(this, "products", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      tableName: "products",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const stockTable = new Table(this, "stock", {
+      partitionKey: {
+        name: "productId",
+        type: AttributeType.STRING,
+      },
+      tableName: "stock",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
     // Create our API Gateway
     const api = new RestApi(this, "ProductsRestAPI", {
@@ -23,13 +43,28 @@ export class ProductServiceStack extends cdk.Stack {
       runtime: Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset("lambda-functions"),
       handler: "getProductsList.handler",
+      environment: {
+        PRODUCTS_TABLE_NAME: productsTable.tableName,
+        STOCK_TABLE_NAME: stockTable.tableName,
+      },
     });
 
     const productLambda = new NodejsFunction(this, "ProductLambda", {
       runtime: Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset("lambda-functions"),
       handler: "getProductsById.handler",
+      environment: {
+        PRODUCTS_TABLE_NAME: productsTable.tableName,
+        STOCK_TABLE_NAME: stockTable.tableName,
+      },
     });
+
+    // Grant our Lambda functions access to our DynamoDB table
+    productsTable.grantReadWriteData(productsLambda);
+    productsTable.grantReadWriteData(productLambda);
+
+    stockTable.grantReadWriteData(productsLambda);
+    stockTable.grantReadWriteData(productLambda);
 
     // Define our API Gateway endpoints
     const products = api.root.addResource("products");
