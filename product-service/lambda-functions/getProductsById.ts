c09edbd35918f1d2e8be89d7f20dd8fd3a4629f9
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-23 16:21:31 +0500

    feat: connect get product by id to dynamodb

--- product-service/lambda-functions/getProductsById.ts
+++ product-service/lambda-functions/getProductsById.ts
@@ -1,18 +1,64 @@
-import { products } from "./products";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
 
-exports.handler = async (event: { pathParameters: { id: string } }) => {
-  const id = event.pathParameters?.id;
+const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME || "";
+const STOCK_TABLE_NAME = process.env.STOCK_TABLE_NAME || "";
 
-  const product = products.find((item) => item.id === id);
-  if (!product) {
+const db = DynamoDBDocument.from(new DynamoDB());
+
+exports.handler = async (event: APIGatewayProxyEvent) => {
+  const requestedItemId = event.pathParameters?.id;
+
+  if (!requestedItemId)
     return {
-      statusCode: 404,
-      body: JSON.stringify({ message: "Product not found" }),
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Error: You are missing the path parameter id",
+      }),
+    };
+
+  const productsParams = {
+    TableName: PRODUCTS_TABLE_NAME,
+    Key: {
+      id: requestedItemId,
+    },
+  };
+
+  const stockParams = {
+    TableName: STOCK_TABLE_NAME,
+    Key: {
+      productId: requestedItemId,
+    },
+  };
+
+  try {
+    const productRes = await db.get(productsParams);
+    const stockRes = await db.get(stockParams);
+    const product = productRes.Item;
+    const stock = stockRes.Item;
+
+    if (!product)
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "Product not found" }),
+      };
+
+    const count = !stock ? 0 : stock.count;
+
+    const productWithCount = {
+      ...product,
+      count,
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(productWithCount),
+    };
+  } catch (dbError) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: `DynamoDB Error: ${dbError}` }),
     };
   }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(product),
-  };
 };
