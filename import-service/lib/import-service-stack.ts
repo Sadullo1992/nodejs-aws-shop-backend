a498ad80225a9e793ac86cf98c7119ea17b33d41
Sadullo <sadulloburiyev1992@gmail.com>
2024-06-30 10:56:57 +0500

    feat: implement getSignedUrl logic

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,60 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { aws_s3 as s3 } from "aws-cdk-lib";
+import { Cors, LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName = "aws-shop-task-5-bucket-csv";
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Bucket from AWS Console
+    const bucket = s3.Bucket.fromBucketName(
+      this,
+      "ImportServiceBucket",
+      bucketName
+    );
+
+    // Create our API Gateway
+    const api = new RestApi(this, "ImportProductsRestAPI", {
+      restApiName: "ImportProductsRestAPI",
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+    });
+
+    // Create lambda function
+    const importProductsLambda = new NodejsFunction(
+      this,
+      "ImportProductsLambda",
+      {
+        runtime: Runtime.NODEJS_20_X,
+        code: lambda.Code.fromAsset("lambda-functions"),
+        handler: "importProductsFile.handler",
+        environment: {
+          BUCKET_NAME: bucket.bucketName,
+        },
+      }
+    );
+
+    // Allow lambda function to bucket
+    bucket.grantPut(importProductsLambda);
+    bucket.grantReadWrite(importProductsLambda);
+
+    // Define our API Gateway endpoints
+    const importProductsResource = api.root.addResource("import");
+
+    // Connect our Lambda functions to our API Gateway endpoints
+    const importProductsIntegration = new LambdaIntegration(
+      importProductsLambda
+    );
+
+    // Define our API Gateway methods
+    importProductsResource.addMethod('GET', importProductsIntegration);
   }
 }
